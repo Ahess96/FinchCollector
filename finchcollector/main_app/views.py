abe34e51bb564b5c0358f0342e24a451f59bad2a
Ahess96 <austinhess1996@comcast.net>
2023-05-02 14:18:28 -0700

    Complete part 2

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-    {"breed": "Zebra Finch", "name": "Eric", "description": "Small, social finch with distinct black and white striped markings."},
-    {"breed": "Gouldian Finch", "name": "Vilem", "description": "Colorful, social finch with bright red, green, and blue markings."},
-    {"breed": "Society Finch", "name": "Kendall", "description": "Easy to care for, social finch with a variety of color mutations."},
-    {"breed": "Goldfinch", "name": "Anthony", "description": "Small, songbird with bright yellow plumage and distinctive song."},
-    {"breed": "European Greenfinch", "Justin": "Ernie", "description": "Medium-sized finch with olive-green plumage and a melodious song."}
-]
+# finches = [
+#     {"breed": "Zebra Finch", "name": "Eric", "description": "Small, social finch with distinct black and white striped markings."},
+#     {"breed": "Gouldian Finch", "name": "Vilem", "description": "Colorful, social finch with bright red, green, and blue markings."},
+#     {"breed": "Society Finch", "name": "Kendall", "description": "Easy to care for, social finch with a variety of color mutations."},
+#     {"breed": "Goldfinch", "name": "Anthony", "description": "Small, songbird with bright yellow plumage and distinctive song."},
+#     {"breed": "European Greenfinch", "Justin": "Ernie", "description": "Medium-sized finch with olive-green plumage and a melodious song."}
+# ]
 
 
 # Create your views here.
@@ -17,6 +18,11 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
