2e6cf8820c889c6d3c49cc14381fccb88a4c1bcc
Ahess96 <austinhess1996@comcast.net>
2023-05-03 22:51:56 -0700

    Finish part 3/4

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Finch
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -17,7 +18,9 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+    #instantiate Feedingform to be rendered
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
 
 class FinchCreate(CreateView):
     model = Finch
@@ -30,3 +33,15 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
