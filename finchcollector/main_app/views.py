714af0e5192a31afcfafe167b9268f17c1ea9eae
Ahess96 <austinhess1996@comcast.net>
2023-05-03 17:07:07 -0700

    Complete 4.5

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
-
-# finches = [
-#     {"breed": "Zebra Finch", "name": "Eric", "description": "Small, social finch with distinct black and white striped markings."},
-#     {"breed": "Gouldian Finch", "name": "Vilem", "description": "Colorful, social finch with bright red, green, and blue markings."},
-#     {"breed": "Society Finch", "name": "Kendall", "description": "Easy to care for, social finch with a variety of color mutations."},
-#     {"breed": "Goldfinch", "name": "Anthony", "description": "Small, songbird with bright yellow plumage and distinctive song."},
-#     {"breed": "European Greenfinch", "Justin": "Ernie", "description": "Medium-sized finch with olive-green plumage and a melodious song."}
-# ]
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def home(request):
@@ -26,3 +18,15 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
